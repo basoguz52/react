3069d55f2595fbe6291991c942e0e3b11fdc19d3
Oğuzhan Baş <oguzhan.bas@bil.omu.edu.tr>
2024-01-23 15:11:06 +0300

    Componentlar kullanıldı

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import User from './components/User';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+
+    return (
+      <div className="container">
+        <Navbar title="User App" />
+        <hr/>
+        <User name = "Oğuzhan Baş" salary = "30000" department = "Bilişim"/>
+        <User name = "Mustafa Murat Coşkun" salary = "5000" department = "Bilişim"/>
+        <User name = "Özge Demir" salary = "5000" />
+      </div>
+    );
+  }
 }
 
 export default App;
