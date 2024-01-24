97741ca6dbf743561e8e35e2c8d5c6bf2c2b346e
Oğuzhan Baş <oguzhan.bas@bil.omu.edu.tr>
2024-01-24 14:54:39 +0300

    componentlar arası iletişim

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
 import React, { Component } from 'react';
-import User from './components/User';
 import Navbar from './components/Navbar';
+import Users from './components/Users';
 
 class App extends Component {
+
+  state = {
+    users: [
+      {
+        id:1,name:"Oğuzhan Baş",salary:"30000",department:"Bilişim"
+      },
+      {
+        id:2,name:"Mustafa Murat Coşkun",salary:"5000",department:"Bilişim"
+      },
+      {
+        id:3,name:"Özge Demir",salary:"6000",department:"Pazarlama"
+      }
+
+    ]
+  }
+
   render() {
 
     return (
       <div className="container">
         <Navbar title="User App" />
-        <hr/>
-        <User name = "Oğuzhan Baş" salary = "30000" department = "Bilişim"/>
-        <User name = "Mustafa Murat Coşkun" salary = "5000" department = "Bilişim"/>
-        <User name = "Özge Demir" salary = "5000" />
+        <hr />
+        <Users users= {this.state.users}/>
       </div>
     );
   }
